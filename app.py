b2bfbdfb8eed62985c1c5ee6a6ac1e0690065289
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-09-26 19:59:23 -0400

    ports

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, redirect
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def hello_world():
 	return '"Mercer" App! #fire'
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
