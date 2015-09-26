451b9653aaa3121847d71718bf1aa7761d051688
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-09-26 19:51:07 -0400

    fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '"Mercer" App! #fire'
+	return '"Mercer" App! #fire'
 
 if __name__ == '__main__':
-	    app.run()
+	app.run()
