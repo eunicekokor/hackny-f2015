bf220560831a6810cb50c66a42db97e5ee8609af
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-09-26 23:47:25 -0400

    stuff that sometimes works

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import requests
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
+from instagram.client import InstagramAPI
 app = Flask(__name__)
 
 client_id = "6c6cebd9c0b64628b6bbdb82b402577a"
@@ -8,16 +9,48 @@ app_uri = "http://mercer.herokuapp.com"
 
 base_url = "https://instagram.com/oauth/authorize/?client_id="+ client_id + "&amp;redirect_uri=" + app_uri + "&amp;response_type=token"
 
-@app.route('/test-auth')
-def test():
-  return redirect(base_url)
+instaConfig = {
+  'client_id':os.environ.get('CLIENT_ID'),
+  'client_secret':os.environ.get('CLIENT_SECRET'),
+  'redirect_uri' : os.environ.get('REDIRECT_URI')
+}
+api = InstagramAPI(**instaConfig)
+
+instagram_access_token = ""
+
+@app.route('/callback')
+def main():
+  media = api.media_popular(count=20)
+  final_media = []
+  print "we are here"
+  for media in popular_media:
+    print media.images['standard_resolution'].url
+  return media
+  # print "holla!!!!"
+  # url = api.get_authorize_url(scope=["likes","comments"])
+  # thing = requests.get(url)
+  # print thing.json()
 
 @app.route('/')
 def hello_world():
-  print url_for('hello_world')
-  access_code = requests.get(base_url)
-  print access_code.text
-  return '"Mercer" App! #fire'
+  print "uhhhhhhh"
+  if instagram_access_token:
+    return "We got a token"
+  print "okay"
+  # if instagram info is in session variables, then display user photos
+  # if 'instagram_access_token' in session and 'instagram_user' in session:
+  #   userAPI = InstagramAPI(access_token=session['instagram_access_token'])
+  #   recent_media, next = userAPI.user_recent_media(user_id=session['instagram_user'].get('id'),count=25)
+
+  #   templateData = {
+  #     'size' : request.args.get('size','thumb'),
+  #     'media' : recent_media
+  #   }
+  #   print "TEST"
+
+  return redirect('/callback')
+
+
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
